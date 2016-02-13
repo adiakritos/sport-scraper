235da1766a62fbace37de283b1f968654c814307
Nick <adiakritos@gmail.com>
2016-02-12 16:25:21 -0800

    update scraper.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,21 +4,41 @@ require 'capybara/poltergeist'
 require 'open-uri'
 require 'pry'
 
+
+module ScrapWrap
+  require 'capybara'
+  require 'capybara/poltergeist'
+
+  def new_session
+    Capybara.register_driver :poltergeist do |app|
+      Capybara::Poltergeist::Driver.new(app, js_errors: false)
+    end
+
+    Capybara.default_driver   = :poltergeist
+
+    @session = Capybara.current_session
+  end
+
+  def html
+    @session.html
+  end
+
+end
+
+
 class Scraper
+  include ScrapWrap
 
-  def go
-
-   c = Capybara.default_driver = :poltergeist
-
-   #doc = Nokogiri::HTML.parse(bwser.html)
-
-   #table = doc.css('.table-main')
-   #games = table.css('.soccer')
-
+  def initialize(url)
+    @url = url
+    browser = new_session
+    browser.visit @url
+    games = browser.all '.soccer'
     binding.pry
   end
 
 end
 
-s = Scraper.new
-s.go
+hello = Scraper.new('http://flashscore.com')
+
+
